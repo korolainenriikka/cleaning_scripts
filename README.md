f0559e1b18cffd39d37f1f0faa96d016ebf2f1b8
jupste <jussi.steenari@helsinki.fi>
2021-08-10 10:23:21 +0300

    MODIFY: refactored script structure ADD: Statistic generation, config.ini file
    Signed-off-by: jupste <jussi.steenari@helsinki.fi>

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 # Tool for data cleaning
 
-This tool is meant to be used to clean AIS data in a machine learning pipeline. The script removes all time/id duplicates from the data and all the rows that have missing coordinate values. It also calculates the speeds for each time entry based on previous coordinates and difference in time. 
+This tool is meant to be used to clean AIS data in a machine learning pipeline. The script removes all time/id duplicates from the data and all the rows that have missing coordinate values. It also differentiates distict legs based when there is a timegap in the data. After this it calculates the speeds for each time entry based on previous coordinates and difference in time. 
 
 
 ### Usage
 
-Run this script from the command line by command `python3 wrangle_script.py <filepath>`. Filepath is the path to the file to be wrangled
+Run this script from the command line by command `python3 wrangle_script.py`. Define the filepath and column names in the config.ini file
 
-This script expects that the dataframe will have columns named 'id', 'lat', 'lon' and 't'
+The config file also takes some parameters for the wrangling:
 
-If the columns are named differently you can input the alternative names using --column column_in_df syntax
+- min_rows : the minimum number of rows allowed for a given leg. Legs with fewer rows than this are removed from the data set.
+- max_speed : the maximum speed allowed for a ship. Faster ships than this are considered outliers and aree removed from the data set. 
+- leg_gap : the time gap needed to differentiate a new leg in seconds
+- fix_values : if True fix the coordinates of the rows with outlier speed based on rolling average. Otherwise remove them
 
-So for example if the dataframe would have id in a column called mmsi you would run the script with
-
-`python3 wrangle_script.py <filepath> --id mmsi`
-
-Also flags -b and -f are used to further wrangle the data. Flag -b calculates the bearing in each point and -f flag does not remove the outliers from the data and instead imputs values using rolling average method.
+The script itself can be used with two flags:
+- -v to print out the program output
+- -s to calculate statistics on the transformation
 
 
 ### Example use
@@ -25,5 +26,7 @@ Download dataset from https://zenodo.org/record/3754481 and unzip the file 'ais.
 
 Use script with command 
 
-`python3 wrangle_script.py ais.csv --id shipid`
+`python3 wrangle_script.py` 
+
+If using a different dataset change the `config.ini` file accordingly
 
--- wrangle_script.py
+++ wrangle_script.py
@@ -5,16 +5,28 @@ from pandas.api.types import is_numeric_dtype, is_datetime64_any_dtype
 import sys
 import argparse
 import matplotlib.pyplot as plt
+import logging
+from configparser import ConfigParser
 
-def calculate_statistics(df):
+
+def calculate_statistics(old_df, new_df):
     print("+++ CALCULATING STATISTICS +++")
+    print("+++ ORIGINAL DATAFRAME +++")
+    print(old_df.describe())
+    print("+++ WRANGLED DATAFRAME +++")
+    print(new_df.describe())
+    show_percentage_of_rows_with_zero_diff_per_leg(old_df, 'id', "Raw dataframe groupby id")
+    show_percentage_of_rows_with_zero_diff_per_leg(new_df, 'id', "Wrangled dataframe groupby id")
+    show_percentage_of_rows_with_zero_diff_per_leg(new_df, 'leg_num', 'Wrangled dataframe groupby leg id')
+
+def show_percentage_of_rows_with_zero_diff_per_leg(df, groupby_id, title):
     df = df.sort_values('t')
-    df['dlat'] = df.groupby('id').lat.diff()
-    df['dlon'] = df.groupby('id').lon.diff()
+    df['dlat'] = df.groupby(groupby_id).lat.diff()
+    df['dlon'] = df.groupby(groupby_id).lon.diff()
     lat_in_top_bin = []
     lon_in_top_bin = []
-    for shipid in df['id'].unique():
-        d = df[df.id==shipid].dropna(subset=['lat', 'lon'], how='any')
+    for g_id in df[groupby_id].unique():
+        d = df[df[groupby_id]==g_id].dropna(subset=['lat', 'lon'], how='any')
         len_lat = len(d.dlat)
         len_lon = len(d.dlon)
         if len_lat < 1 or len_lon < 1:
@@ -24,6 +36,7 @@ def calculate_statistics(df):
     fig, (ax1, ax2) = plt.subplots(1,2, figsize = (20,20))
     lat_in_top_bin = sorted(lat_in_top_bin, key=lambda x: x[0])
     lon_in_top_bin = sorted(lon_in_top_bin, key=lambda x: x[0])
+    fig.suptitle(title)
     ax1.set_xlabel('number of records')
     ax1.set_title('Delta latitude')
     ax1.set_ylabel('percentage of zero diff rows')
@@ -36,47 +49,57 @@ def calculate_statistics(df):
     ax2.scatter(b[0], b[1]*100, color="blue")
     plt.show()
 
-def wrangle_dataset(filepath, id='id', lon='lon', lat='lat', time='t', fix=False, bearing=False):
-    # Load dataframe
-    print("+++ START +++\n")
-    print("+++ READING DATAFRAME FROM FILE +++")
+def load_dataset():
+    '''Loads the dataset. Then removes all id/time dublicates and rows with missing coordinates. Return dataframe with renamed columns '''
+    dataframe_args = dict(config_object['DATAFRAME'])
+    filepath, id_column, lon_column, lat_column, time_column = dataframe_args.values()
+    logging.info("+++ START +++\n+++ READING DATAFRAME FROM FILE +++")
     df = pd.read_csv(filepath)
     original_length = len(df)
-    
-    print("+++ ORIGINAL LENGTH: ", original_length, "+++\n")
-    
-    # Speed calculation function
-    def calculate_speed(timedelta):
-        lat1, lon1, lat2, lon2 = map(np.radians, [df.loc[timedelta.index, 'lat'], df.loc[timedelta.index, 'lon'], df.loc[timedelta.index, 'lat_prev'], df.loc[timedelta.index, 'lon_prev']])
-        # haversine formula
-        dlon = lon2 - lon1 
-        dlat = lat2 - lat1
-        a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
-        c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a)) 
-        r = 6371000 # Radius of earth in meters. Use 3956 for miles
-        mps_to_knots = 1.944 # 1 meter per second is about 1.944 knots 
-        return mps_to_knots*((c * r) / timedelta)
-    
-    # Bearing calculation function
-    def calculate_bearing(lat):
-        dlon = np.absolute(df.loc[lat.index, 'lon'] - df.loc[lat.index, 'lon_prev'])
-        X = np.cos(df.loc[lat.index, 'lat_prev'])* np.sin(dlon)
-        Y = np.cos(lat) * np.sin(df.loc[lat.index, 'lat_prev']) - np.sin(lat) * np.cos(df.loc[lat.index, 'lat_prev']) * np.cos(dlon)
-        return np.degrees(np.arctan2(X,Y))
-    
-    # Drop time/id dublicates
-    print("+++ DROPPING ID/TIME DUBLICATES +++")
-    df = df.drop_duplicates([time, id])
+    logging.info("+++ ORIGINAL LENGTH: " + str(original_length) + "+++\n")
+    logging.info("+++ DROPPING ID/TIME DUBLICATES +++")
+    df = df.drop_duplicates([time_column, id_column])
     prev_len = len(df)
-    print("+++ ROWS DROPPED:", original_length-prev_len, "+++\n")
+    logging.info("+++ ROWS DROPPED: " + str(original_length-prev_len) + "+++\n")
     # Standardise column names
-    df = df.rename(columns={id:'id', lon: 'lon', lat: 'lat', time:'t'})
+    df = df.rename(columns={id_column:'id', lon_column: 'lon', lat_column: 'lat', time_column:'t'})
     # Drop rows that are missing coordinates
-    print("+++ DROPPING ROWS WHERE COORDINATES ARE MISSING +++")
+    logging.info("+++ DROPPING ROWS WHERE COORDINATES ARE MISSING +++")
+    prev_len = len(df)
     df = df.dropna(subset=['lon', 'lat'], how='any')
-    print("+++ ROWS DROPPED:", prev_len- len(df), "+++\n")
-    prev_len=len(df)
-    # Calculate timedeltas
+    logging.info("+++ ROWS DROPPED:" + str(prev_len-len(df)) + "+++\n")
+    return df
+
+def drop_small_legs(df):
+    ''' If timedelta between two measurements is more than the threshold defined in config file  '''
+    interm = df[(df.timedelta > int(config_object['WRANGLING']['leg_gap'])) | df.timedelta.isna()].reset_index()['index']
+    df['leg_num'] = pd.Series(interm.index, index=interm)
+    df['leg_num'] = df.groupby('id').leg_num.fillna(method='ffill')
+    logging.info("+++ DROPPING SHIPS THAT HAVE FEWER THAN " + config_object['WRANGLING']['min_rows'] + " MEASUREMENTS +++")
+    prev_len = len(df)
+    df = df[df.groupby('id')['id'].transform('size')>int(config_object['WRANGLING']['min_rows'])]
+    logging.info("+++ ROWS DROPPED: " + str(prev_len-len(df)) + " +++\n")
+    return df
+
+def fix_values(df):
+    if bool(config_object['WRANGLING']['fix_values']):
+        ship_legs = df.groupby('leg_num')
+        logging.info("+++ FIXING SPEED VALUES OUTLIERS +++")
+        df.lon = np.where(df['speed']>25, np.nan, df.lon)
+        df.lat = np.where(df['speed']>25, np.nan, df.lat)
+        logging.info("+++ TOTAL OF OUTLIERS: LAT-" + str(len(df[df.lat.isna()])) + " LON-" + str(len(df[df.lon.isna()])))
+        rolling_averages_lat = ship_legs.lat.rolling(10, min_periods=1, center=True).mean()
+        rolling_averages_lon = ship_legs.lon.rolling(10, min_periods=1, center=True).mean()
+        df.lat.fillna(rolling_averages_lat.reset_index(level=0)['lat'])
+        df.lon.fillna(rolling_averages_lon.reset_index(level=0)['lon'])
+        return df
+    else:
+        logging.info("+++ DROPPING SPEED VALUE OUTLIERS +++")
+        df = df[df['speed']<int(config_object['WRANGLING']['max_speed'])]
+        return df
+
+def timedelta(df):
+    '''Method for calculating the timedelta between each entry within a ship'''
     if is_numeric_dtype(df['t']):
         # Sort values by timestamp
         df = df.sort_values('t')
@@ -90,47 +113,67 @@ def wrangle_dataset(filepath, id='id', lon='lon', lat='lat', time='t', fix=False
         df = df.sort_values('t')
         ships  = df.groupby('id')
         df['timedelta'] = ships['t'].diff().transform(lambda x: abs(x.total_seconds()))
-    
-    # Differentiate the legs
-    interm = df[(df.timedelta > (30*60)) | df.timedelta.isna()].reset_index()['index']
-    df['leg_num'] = pd.Series(interm.index, index=interm)
-    df['leg_num'] = df.groupby('id').leg_num.fillna(method='ffill')
-    # Calculate speeds
+    return df
+
+def calculate_speed(df):
+    '''Calculate speed using timedeltas and haversine distance between two points. Uses auxiliarry calculate_speed function for fast axis wise application'''
+    # Auxiliary function
+    def calculate_speed_aux(timedelta):
+        lat1, lon1, lat2, lon2 = map(np.radians, [df.loc[timedelta.index, 'lat'], df.loc[timedelta.index, 'lon'], df.loc[timedelta.index, 'lat_prev'], df.loc[timedelta.index, 'lon_prev']])
+        # haversine formula
+        dlon = lon2 - lon1 
+        dlat = lat2 - lat1
+        a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+        c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a)) 
+        r = 6371000 # Radius of earth in meters. Use 3956 for miles
+        mps_to_knots = 1.944 # 1 meter per second is about 1.944 knots 
+        return mps_to_knots*((c * r) / timedelta)
+    ships = df.groupby('id')
     df['lon_prev'] = ships.lon.shift(1)
     df['lat_prev'] = ships.lat.shift(1)
-    df['speed'] = df.groupby('leg_num')['timedelta'].apply(calculate_speed)
+    df['speed'] = ships.timedelta.apply(calculate_speed_aux)
+    return df
+
+def wrangle_dataset(df):
+    # Bearing calculation function
+    def calculate_bearing(lat):
+        dlon = np.absolute(df.loc[lat.index, 'lon'] - df.loc[lat.index, 'lon_prev'])
+        X = np.cos(df.loc[lat.index, 'lat_prev'])* np.sin(dlon)
+        Y = np.cos(lat) * np.sin(df.loc[lat.index, 'lat_prev']) - np.sin(lat) * np.cos(df.loc[lat.index, 'lat_prev']) * np.cos(dlon)
+        return np.degrees(np.arctan2(X,Y))
+
+    original_length=len(df)
+    # Calculate timedeltas
+    df = timedelta(df)
+    # Differentiate the legs
+    df = drop_small_legs(df)
+    # Remove rows where the speed is above limit
+    
+    df = calculate_speed(df)
+
+    df = fix_values(df)
+    '''
     if bearing:
         df['bearing'] = df.groupby('leg_num')['lat'].apply(calculate_bearing)
-    if fix:
-        print("+++ FIXING SPEED VALUES OUTLIERS +++")
-        df.lon = np.where(df['speed']>25, np.nan, df.lon)
-        df.lat = np.where(df['speed']>25, np.nan, df.lat)
-        print("+++ TOTAL OF OUTLIERS: LAT-", len(df[df.lat.isna()]), "LON-", len(df[df.lon.isna()]))
-        rolling_averages_lat = ships.lat.rolling(10, min_periods=1, center=True).mean()
-        rolling_averages_lon = ships.lon.rolling(10, min_periods=1, center=True).mean()
-        df.lat.fillna(rolling_averages_lat.reset_index(level=0)['lat'])
-        df.lon.fillna(rolling_averages_lon.reset_index(level=0)['lon'])
-        return df
-    # Do the maskings for outlier speeds
-    print("+++ DROPPING SPEED VALUE OUTLIERS +++")
-    df = df[df['speed']<25]
-    print("+++ ROWS DROPPED:", prev_len - len(df),"+++\n")
-    print("+++ TOTAL ROWS DROPPED:", original_length-len(df), "+++")
-    print("---DONE---")
+    '''
+    logging.info("+++ TOTAL ROWS DROPPED: " + str(original_length-len(df)) + " +++")
+    logging.info("---DONE---")
     return df
 
 parser = argparse.ArgumentParser(description="Tool for data wrangling")
-parser.add_argument('filepath', help='filepath of data to be processed')
-parser.add_argument('-i','--id', help='name of id column if other than "id"', default='id')
-parser.add_argument('-x','--lon', help='name of lon column if other than "lon"', default='lon')
-parser.add_argument('-y','--lat', help='name of lat column if other than "lat"',default='lat')
-parser.add_argument('-t','--time', help='name of time column if other than "time"', default='t')
-parser.add_argument('-f','--fix', help='use flag to fix outlier values using rolling average', default=False, action="store_true")
-parser.add_argument('-b', '--bearing', help='use flag to calculate bearing', default=False, action="store_true")
-parser.add_argument('-s', '--statistics', help='use flag to calculate statistics', default=False, action="store_true")
-args=vars(parser.parse_args())
-statistics = args.pop('statistics')
-df = wrangle_dataset(**args)
-if statistics:
-    calculate_statistics(df)
-df.to_csv("out_" + args['filepath'])
+parser.add_argument('-v', '--verbose', help = 'show program output', default=False, action="store_true")
+parser.add_argument('-s', '--statistics', help='calculate statistics on the transformation', default=False, action="store_true")
+
+config_object = ConfigParser()
+config_object.read("config.ini")
+cli_args=vars(parser.parse_args())
+if cli_args['verbose']:
+    logging.basicConfig(level=logging.DEBUG)
+
+raw_dataframe = load_dataset()
+
+fixed_dataframe = wrangle_dataset(raw_dataframe)
+
+if cli_args['statistics']:
+    calculate_statistics(raw_dataframe, fixed_dataframe)
+fixed_dataframe.to_csv("out_" + config_object['DATAFRAME']['filepath'])
